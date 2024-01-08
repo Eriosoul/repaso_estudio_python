b67c2e1dac02c335411a112f040efd9796140370
Eriosoul <tboss1780@gmail.com>
2024-01-08 23:24:53 +0100

    call to main

--- Scrapp/main.py
+++ Scrapp/main.py
@@ -2,3 +2,13 @@
 #  https://es.wikipedia.org/wiki/Anexo:Tenista_l%C3%ADder_en_la_Clasificaci%C3%B3n_de_la_ATP_individual
 
 #  Vamos a otener la informacion de los tenistas lideres en la calisicaion de la ATP según wikipedia
+
+from templates.spider import TenistaLider
+
+if __name__ == '__main__':
+    t: TenistaLider = TenistaLider()
+    try:
+        t.check_url_status()
+        t.get_data_from_first_table()
+    except Exception as e:
+        print("Error: ", e)
