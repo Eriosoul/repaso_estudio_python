525e3d019e67a752007c11aa728de901ae82ee89
Eriosoul <tboss1780@gmail.com>
2024-01-31 23:35:21 +0100

    the data don't append

--- Ejercicios/reto_12/templates/system.py
+++ Ejercicios/reto_12/templates/system.py
@@ -1,3 +1,5 @@
+import json
+
 from book import Book
 from library_system import LibrarySystem
 from Ejercicios.reto_12.check_book_data import CheckBookData
@@ -19,8 +21,37 @@ class SystemBook:
             self.author_book = str(input("Introduce el autor de dicho libro: "))
             self.available_book = int(input("Introduce la cantidad de libros nuevos: "))
             print(self.title_book, self.author_book, self.available_book)
+            return self.title_book, self.author_book, self.available_book
         except Exception as e:
             print(e)
+
+    def struct_add_book(self):
+        my_struct = {
+            "Nombre del libro": self.title_book,
+            "Autor del libro": self.author_book,
+            "Disponibilidad": self.available_book
+        }
+        try:
+            # Verificar si el archivo JSON existe
+            if not self.check_book.exist():
+                print("No se pudo verificar la existencia del archivo JSON.")
+                return
+
+            # Obtener la lista de libros del archivo JSON
+            with open(self.check_book.library_book, "r") as file:
+                book_list = json.load(file)
+
+            # Agregar el nuevo libro a la lista
+            book_list.append(my_struct)
+
+            # Escribir la lista actualizada al archivo JSON
+            with open(self.check_book.library_book, "a") as file:
+                json.dump(book_list, file, indent=4)
+
+            print("¡Libro agregado correctamente!")
+
+        except Exception as e:
+            print("Error al agregar el libro:", e)
 
 
 def menu():
@@ -45,5 +76,7 @@ if __name__ == '__main__':
     option = menu()
     if option == 1:
         l.add_book()
+        print("mostramos informacion ")
+        l.struct_add_book()
     else:
         print("error")
