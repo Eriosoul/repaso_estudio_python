c40f42b76a5d4a88900a5c2968cbdf0416336a5f
Eriosoul <tboss1780@gmail.com>
2024-02-09 00:00:01 +0100

    getting data

--- Ejercicios/reto_13/request.py
+++ Ejercicios/reto_13/request.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 from requests import Response
+from bs4 import BeautifulSoup
 
 
 class GlobalSongs:
@@ -11,13 +12,23 @@ class GlobalSongs:
 
     def get_status(self):
         try:
-            r: Response = requests.get(self.url)
-            if r.status_code != 200:
-                print("Error al obtener los datos de la web", r)
+            self.r: Response = requests.get(self.url)
+            if self.r.status_code != 200:
+                self.r.raise_for_status()
+                print("Error al obtener los datos de la web", self.r)
             else:
                 print("Conexion exitosa")
+                print(self.r.text)
+                self.get_data()
+        except requests.exceptions.HTTPError as err:
+            print(f"Error HTTP: {err}")
         except Exception as e:
             print("Error al obtener los datos: ", e)
+
+    def get_data(self):
+        soup: BeautifulSoup = BeautifulSoup(self.r.text, "html.parser")
+        data = soup.find('div', class_='h4HgbO_Uu1JYg5UGANeQ.wTUruPetkKdWAR1dd6w4')
+        print(data.text)
 
 
 if __name__ == '__main__':
