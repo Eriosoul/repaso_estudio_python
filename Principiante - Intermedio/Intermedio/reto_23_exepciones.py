f9b6ab30c7b83e9fc05624f68562a7b82e263287
Eriosoul <tboss1780@gmail.com>
2024-04-03 18:55:56 +0200

    manejo de exepciones y finally

--- Principiante - Intermedio/Intermedio/reto_23_exepciones.py
+++ Principiante - Intermedio/Intermedio/reto_23_exepciones.py
@@ -23,10 +23,43 @@ def division():
         div = 10 / 0
         return div
     except Exception as e:
-        print("Error: ", e)
+        print(f"Error: ({type(e).__name__})")
         return None
 
 
 division()
 
 
+class StrExeption(Exception):
+    pass
+
+
+def procesar_parametros(parametros: list):
+    if len(parametros) < 6:
+        raise IndexError()
+    elif parametros[1] == 0:
+        raise ZeroDivisionError()
+    elif type(parametros[2]) == str:
+        raise StrExeption(
+            "El elemento 3 disponde de una cadena de texto"
+        )
+
+    print(parametros[2])
+    print(parametros[0]/parametros[2])
+    print(parametros[2] + 5)
+
+
+try:
+    procesar_parametros([1, 2, "Erio", 4, 5, 6])
+except IndexError as e:
+    print("El numero debe ser mayor aque 2")
+except ZeroDivisionError as e:
+    print("No se puede dividir por 0", e)
+except StrExeption as e:
+    print(e)
+except Exception as e:
+    print("Error: ", e)
+else:
+    print("No se ha producido ningun error")
+finally:
+    print("El programa ha finalizado")
