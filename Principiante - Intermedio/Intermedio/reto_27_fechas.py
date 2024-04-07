911eabc13ae5ccaac72d9312f81918bfcb2c695d
Eriosoul <tboss1780@gmail.com>
2024-04-08 00:01:20 +0200

    datetime

---
+++ Principiante - Intermedio/Intermedio/reto_27_fechas.py
@@ -0,0 +1,40 @@
+"""
+* EJERCICIO:
+ * Crea dos variables utilizando los objetos fecha (date, o semejante) de tu lenguaje:
+ * - Una primera que represente la fecha (día, mes, año, hora, minuto, segundo) actual.
+ * - Una segunda que represente tu fecha de nacimiento (te puedes inventar la hora).
+ * Calcula cuántos años han transcurrido entre ambas fechas.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Utilizando la fecha de tu cumpleaños, formatéala y muestra su resultado de
+ * 10 maneras diferentes. Por ejemplo:
+ * - Día, mes y año.
+ * - Hora, minuto y segundo.
+ * - Día de año.
+ * - Día de la semana.
+ * - Nombre del mes.
+ * (lo que se te ocurra...)
+"""
+import datetime
+from datetime import timedelta
+
+delta = timedelta(
+    days=50,
+    seconds=27,
+    microseconds=10,
+    milliseconds=29000,
+    minutes=5,
+    hours=8,
+    weeks=2
+)
+# Only days, seconds, and microseconds remain
+print(delta)
+
+
+actual_day = datetime.datetime.now()
+print(actual_day)
+
+birthday = datetime.datetime(day=27, month=1, year=1000)
+
+info_day = actual_day - birthday
+print("Diferencia de días:", info_day.days)
