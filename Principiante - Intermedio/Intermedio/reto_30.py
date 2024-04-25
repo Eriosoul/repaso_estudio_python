c150853db771781a96385abd6a7e2bd168a98b59
Eriosoul <tboss1780@gmail.com>
2024-04-25 02:26:08 +0200

    programa de tareas

--- Principiante - Intermedio/Intermedio/reto_30.py
+++ Principiante - Intermedio/Intermedio/reto_30.py
@@ -1,5 +1,6 @@
 """
-Crea un programa en Python que simule un sistema de gestión de tareas. Deberías tener una clase Tarea que represente una tarea en el sistema. La clase Tarea debe tener los siguientes atributos:
+Crea un programa en Python que simule un sistema de gestión de tareas.
+Deberías tener una clase Tarea que represente una tarea en el sistema. La clase Tarea debe tener los siguientes atributos:
 
 nombre: El nombre de la tarea.
 descripcion: Una descripción de la tarea.
@@ -14,3 +15,55 @@ mostrar_tareas(): Para mostrar la información de todas las tareas en el sistema
 
 """
 
+
+class Tarea:
+    def __init__(self, nombre, descripcion, fecha_limite):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.fecha_limite = fecha_limite
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Descripción: {self.descripcion}")
+        print(f"Fecha límite: {self.fecha_limite}")
+
+
+class GestorTareas:
+    def __init__(self):
+        self.tareas = []
+
+    def agregar_tarea(self, nombre, descripcion, fecha_limite):
+        tarea = Tarea(nombre, descripcion, fecha_limite)
+        self.tareas.append(tarea)
+
+    def mostrar_tareas(self):
+        if not self.tareas:
+            print("No hay tareas registradas.")
+        else:
+            print("Lista de tareas:")
+            for tarea in self.tareas:
+                tarea.mostrar_informacion()
+
+
+if __name__ == "__main__":
+    gestor = GestorTareas()
+
+    while True:
+        print("\n1. Agregar tarea")
+        print("2. Mostrar tareas")
+        print("3. Salir")
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            nombre = input("Introduce el nombre de la tarea: ")
+            descripcion = input("Breve descripción de la tarea: ")
+            fecha_limite = input("Fecha límite de la tarea: ")
+            gestor.agregar_tarea(nombre, descripcion, fecha_limite)
+            print("Tarea agregada exitosamente.")
+        elif opcion == "2":
+            gestor.mostrar_tareas()
+        elif opcion == "3":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Inténtelo de nuevo.")
